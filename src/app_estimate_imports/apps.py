658b1b58166a79d6275a8a669e9adc8ac205029d
RomanL91 <dev_roman91@mail.ru>
2025-11-27 06:37:11 +0500

    fix(BX-32997): исправлена опечатка

--- src/app_estimate_imports/apps.py
+++ src/app_estimate_imports/apps.py
@@ -5,4 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class AppEstimateImportsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app_estimate_imports"
-    verbose_name = _("Импорт EXEL | Разметка")
+    verbose_name = _("Импорт EXCEL | Разметка")
